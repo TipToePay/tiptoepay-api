25eb56a75e7f899d62246eb869e92b28d0f235b0
Peter Cho <petercho39@gmail.com>
2018-06-06 21:46:31 -0400

    restructured folder path

--- tiptoepay/urls.py
+++ tiptoepay/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from tiptoepay.testapp import views
+from tiptoepay.main import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
